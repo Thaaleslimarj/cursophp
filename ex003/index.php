ad58355c03a7d858f4f9d109677c7661176eaeea
thaaleslimarj <thaaleslimarj@gmail.com>
2024-09-17 15:02:19 -0300

    evolução

---
+++ ex003/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h3>Teste de tipos primitivos</h3>
+    <?php  
+    // (0x = hexadecimal) (0b = binário) (0 = Octal)
+    //$num = 0x1A;
+    //echo "O valor da variável é $num"; 
+
+    // $v = "Thales";
+    // var_dump($v);
+    
+    //$num = 3e2;
+    //echo "O valor é $num";
+    
+    // $num = (int) 3e2;  
+    // var_dump($num);
+    
+    //$num = (bool) "950";
+    //var_dump($num);
+
+    //$casado = false;
+    //var_dump($casado);
+    //print "O valor para casado é $casado";
+    // true bool é 1 | false bool é vazio (comentário)
+
+    //Array =>  $vet = [6 , 2.5 , "Maria" , 3 , false];
+    //var_dump($vet);
+ 
+    //Object =>  class Pessoa{
+      //    private String $nome;  
+    //}
+
+    //$p = new Pessoa;
+    //var_dump($p);
+    
+    ?>
+</body>
+</html>
